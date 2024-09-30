907f9fdf0818030714c1df0eedc581f741e0a110
Youth-one <o3her456@gmail.com>
2024-09-30 16:04:38 +0900

    feat : member domain making also making repository

--- src/main/java/hello/hello_spring/domain/Member.java
+++ src/main/java/hello/hello_spring/domain/Member.java
@@ -4,4 +4,14 @@ public class Member {
     private Long id;
     private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
 }

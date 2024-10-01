a74d4adda180d88e6a053b4593bdd9c9ad75b201
Youth-one <o3her456@gmail.com>
2024-10-01 23:06:26 +0900

    feat : findById , findByName Test code

--- src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -14,7 +14,22 @@ public class MemoryMemberRepositoryTest {
         repository.save(member);
         Member result = repository.findById(member.getId()).get();
         System.out.println("result = " + (result == member));
-        Assertions.assertEquals(member, null); // result == member 같은지 확인
+        Assertions.assertEquals(member, result); // result == member 같은지 확인
 
+    }
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("sungwon1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("sungwon2");
+        repository.save(member2);
+
+        Member result = repository.findByName("sungwon1").get();
+        System.out.println("result = " + result.getId()+"그 이름은"+result.getName());
+        Member result2 = repository.findByName("sungwon2").get();
+        System.out.println("result2 = " + result2.getName());
     }
 }

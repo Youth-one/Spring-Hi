d997830eb4e3d646cee011a51fc964a36330fbd0
Youth-one <o3her456@gmail.com>
2024-10-03 16:35:24 +0900

    feat : 스프링빈 추가설명

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -1,4 +1,5 @@
 package hello.hello_spring;
+// 여기서 부터.하위 패키지들을 자동적으로 컴포넌트 스캔을 떄려서 애노테이션들을 찾아 스프링빈에 등록해서 관리한다
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

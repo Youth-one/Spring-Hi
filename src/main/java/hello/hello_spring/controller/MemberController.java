d048804359436c4dcfcfdf7e153ef2391790816f
Youth-one <o3her456@gmail.com>
2024-10-03 18:28:03 +0900

    feat : member register , member List check it out develop

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,11 +1,15 @@
 package hello.hello_spring.controller;
 
+
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -35,5 +39,11 @@ public class MemberController {
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
